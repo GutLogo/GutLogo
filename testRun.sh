9393fc14b04fc167cf09204c3b9427857d125655
CharlieL7 <charliel@udel.edu>
2017-01-16 14:55:54 -0500

    edit testRun.sh

--- testRun.sh
+++ testRun.sh
@@ -1,7 +1,7 @@
 #PBS -N gutSim -S /bin/sh
 #PBS -l nodes=1:ppn=16
-java -Xmx1024m -Dfile.encoding=UTF-8 -cp ../netlogo-6.0/app/NetLogo.jar \
-org.nlogo.headless.Main \
---model NLCode.nlogo \
+~/netlogo-6.0/netlogo-headless.sh \
+--model ~/gut-nlogo-model/NLCode.nlogo \
 --experiment testCluster \
 --spreadsheet -
+--threads 16

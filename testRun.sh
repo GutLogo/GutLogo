efb178c3416f6aaedf4eb4ab8c80541c163b25e2
CharlieL7 <charliel@udel.edu>
2017-01-16 14:36:38 -0500

    edit testRun.sh

--- testRun.sh
+++ testRun.sh
@@ -1,5 +1,5 @@
 #PBS -N gutSim -S /bin/sh
-#PBS -l nodes=1:ppn=25
+#PBS -l nodes=1:ppn=16
 java -Xmx1024m -Dfile.encoding=UTF-8 -cp ../netlogo-6.0/app/NetLogo.jar \
 org.nlogo.headless.Main \
 --model NLCode.nlogo \

257b4b74f0eab45a2461912c1bd0d06b6d29c786
Charlie <CharlieLin7@gmail.com>
2017-02-03 14:34:33 -0500

    new checkStable script

--- checkStability.sh
+++
@@ -1,7 +0,0 @@
-#PBS -N gutSim -S /bin/sh
-#PBS -l nodes=1:ppn=40
-~/netlogo-6.0/netlogo-headless.sh \
---model ~/gut-nlogo-model/NLCode.nlogo \
---experiment checkStable \
---table ~/results/stableCheck.csv \
---threads 40
---
+++ checkStable.sh
@@ -0,0 +1,15 @@
+#PBS -N gutSim -S /bin/sh
+#PBS -l nodes=1:ppn=40
+
+# -Xmx1024m                     use up to 1GB RAM (edit to increase)
+# -Dfile.encoding=UTF-8         ensure Unicode characters in model files are compatible cross-platform
+# -classpath app/NetLogo.jar    specify main jar
+# org.nlogo.headless.Main       specify we want headless, not GUI
+# "$@"                          pass along any command line arguments
+
+java -Xmx4096m -Dfile.encoding=UTF-8 \
+-classpath ~/netlogo-6.0/app/netlogo-6.0.0.jar org.nlogo.headless.Main "$@" \
+--model ~/gut-nlogo-model/NLCode.nlogo \
+--experiment checkStable \
+--table ~/results/stableCheck.csv \
+--threads 40

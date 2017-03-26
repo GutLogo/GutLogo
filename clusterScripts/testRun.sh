ea124a45702580a77af960883142d72548942878
CharlieL7 <charliel@udel.edu>
2017-03-25 21:34:58 -0400

    moved scripts to folder

---
+++ clusterScripts/testRun.sh
@@ -0,0 +1,7 @@
+#PBS -N gutSim -S /bin/sh
+#PBS -l nodes=1:ppn=16
+~/netlogo-6.0/netlogo-headless.sh \
+--model ~/gut-nlogo-model/NLCode.nlogo \
+--experiment testCluster \
+--spreadsheet ~/results/spreadsheet-output.csv
+--threads 16

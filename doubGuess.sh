c530abffabe771578e18b0ba53cf9dc2e1814f63
CharlieL7 <charliel@udel.edu>
2017-03-25 21:34:41 -0400

    moved scripts to folder

--- doubGuess.sh
+++
@@ -1,7 +0,0 @@
-#PBS -N gutSim -S /bin/sh
-#PBS -l nodes=1:ppn=40
-~/netlogo-6.0/netlogo-headless.sh \
---model ~/gut-nlogo-model/NLCode.nlogo \
---experiment doubGuess \
---spreadsheet ~/results/doubGuess.csv \
---threads 40
--- testRun.sh
+++
@@ -1,7 +0,0 @@
-#PBS -N gutSim -S /bin/sh
-#PBS -l nodes=1:ppn=16
-~/netlogo-6.0/netlogo-headless.sh \
---model ~/gut-nlogo-model/NLCode.nlogo \
---experiment testCluster \
---spreadsheet ~/results/spreadsheet-output.csv
---threads 16

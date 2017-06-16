435ae3ccccbe89826dd8d41c5bd9b763fb2c22eb
CharlieL7 <CharlieLin7@gmail.com>
2017-06-15 21:29:03 -0400

    added a space to fix

--- junitTests/MakeRun.sh
+++ junitTests/MakeRun.sh
@@ -1,2 +1,2 @@
-java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:.\
+java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:. \
 org.junit.runner.JUnitCore ExampTest

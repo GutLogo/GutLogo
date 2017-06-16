9702fa55e1821a9ff656bbbc0c5ac94e90575981
J <jsculver@udel.edu>
2017-06-16 17:55:29 -0400

    Tests

--- junitTests/MakeRun.sh
+++ junitTests/MakeRun.sh
@@ -1,2 +1,11 @@
-java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:.\
-org.junit.runner.JUnitCore ExampTest
+#java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:. \
+#org.junit.runner.JUnitCore ExampTest
+
+java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:. \
+org.junit.runner.JUnitCore stopCheckTests
+
+java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:. \
+org.junit.runner.JUnitCore onePatchTests
+
+java -Djava.awt.headless=true -cp app/netlogo-6.0.0.jar:junit/junit.jar:junit/hamcrest.jar:. \
+org.junit.runner.JUnitCore twoPatchTests
